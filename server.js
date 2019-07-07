322e98867d16ce543113f2da96beb466d0747306
GunnyD <45931516+DeanJG@users.noreply.github.com>
2019-07-06 22:58:36 -0700

    basic file setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.listen(process.env.PORT || 3001)
